7e50a26f3b1f5e1b3655dbe78a14053e43d424db
Maciej Gierada <maciek.gierada@gmail.com>
2020-07-24 14:10:14 -0700

    updated example input files

--- example_run_files/run_me.sh
+++ example_run_files/run_me.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-module load quantum-espresso/6.4.1-bdw
+# module load quantum-espresso/6.4.1-bdw
+balsam init ~/myWorkflow_bebop
 source balsamactivate ~/myWorkflow_bebop
-#balsam job --name launch_job --workflow test --application Python --args $PWD/run_me.py --yes
-python $PWD/run_me.py
-balsam submit-launch -A CONDO -q csed -t 1200 -n 1 --job-mode=serial
+# export BALSAM_DB_PATH='~/myWorkflow_bebop:$BALSAM_DB_PATH'
+# balsam job --name launch_job --workflow test --application Python3 --args $PWD/run_me.py --yes
+python3 $PWD/run_me.py
+# balsam submit-launch -A CONDO -q day-long-cpu -t 1200 -n 1 --job-mode=serial
+balsam submit-launch -A whatever -q day-long-cpu -t 1200 -n 1 --job-mode=serial --sched-flags="-n 48"
 #--job-mode=mpi
 #watch balsam ls   #  follow status in realtime from co
