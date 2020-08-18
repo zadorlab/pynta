4811f72f6a3a3eecdb0e71675c694909eb62c17c
Maciej Gierada <maciek.gierada@gmail.com>
2020-08-17 17:09:45 -0700

    fixed a bug which return only one error if there are more then one in fact

--- rmgcat_to_sella/check_input.py
+++ rmgcat_to_sella/check_input.py
@@ -30,8 +30,10 @@ class InputChecker():
         self.slab = slab
         self.inputR2S = inputR2S
         self.run_me = run_me
+        # Get the path to the working directory
+        self.working_dir = os.getcwd()
 
-    def check(self):
+    def check_all(self):
         ''' Print info about checking input files '''
         print('Checking input...')
         print('---')
@@ -40,31 +42,58 @@ class InputChecker():
             print('Error')
             print('Make sure all input files are '
                   'in your working directory')
-            exit()
         else:
             print('Passed')
 
     def is_input_file(self):
         ''' Check if there are input files in the working directory '''
-        # Get the path to the working directory
-        working_dir = os.getcwd()
+        # create check list
+        check_list = []
 
         # check inputs
+        check_yaml = self.check_yaml()
+        check_list.append(check_yaml)
+        check_slab = self.check_slab()
+        check_list.append(check_slab)
+        check_inputR2S = self.check_inputR2S()
+        check_list.append(check_inputR2S)
+        check_run_me = self.check_run_me()
+        check_list.append(check_run_me)
+
+        
+        if any(check_list):
+            return False
+        else:
+            return True
+
+    def check_yaml(self):
         if not os.path.isfile(self.yamlfile):
             print('!    .yaml file is not in '
-                  'your working directory: \n{}'.format(working_dir))
+                  'your working directory: \n{}'.format(self.working_dir))
             return False
-        elif not os.path.isfile(self.slab):
+        else:
+            return True
+
+    def check_slab(self):
+        if not os.path.isfile(self.slab):
             print('!    .xyz file with optimized slab is not in '
-                  'your working directory: \n{}'.format(working_dir))
+                  'your working directory: \n{}'.format(self.working_dir))
             return False
-        elif not os.path.isfile(self.inputR2S):
-            print('!    inputR2S.py file is not in '
-                  'your current working directory: \n{}'.format(working_dir))
+        else:
+            return True
+
+    def check_inputR2S(self):
+        if not os.path.isfile(self.inputR2S):
+            print('!    inputR2S.py file is not in your current'
+                  ' working directory: \n{}'.format(self.working_dir))
             return False
-        elif not os.path.isfile(self.run_me):
-            print('!    run_me.py file is not in '
-                  'your current working directory: \n{}'.format(working_dir))
+        else:
+            return True
+
+    def check_run_me(self):
+        if not os.path.isfile(self.run_me):
+            print('!    run_me.py file is not in your current'
+                  ' working directory: \n{}'.format(self.working_dir))
             return False
         else:
             return True
