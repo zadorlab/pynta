ed36866955a168fba20cde8a1cb177a772b1a965
Maciej Gierada <maciek.gierada@gmail.com>
2020-08-21 16:50:29 -0700

    readme update and cleaning example files

---
+++ example_run_files/inputR2S_menten.py
@@ -0,0 +1,95 @@
+from pathlib import Path
+'''
+####################################################
+                    Basic Input
+####################################################
+'''
+####################################################
+# specify the name of the main directory with calculations
+facetpath = 'Cu_100'
+####################################################
+# do you want to run surface optimization
+optimize_slab = True
+####################################################
+# specify name of the slab
+slab_name = 'Cu_100_slab_opt'
+####################################################
+# specify facet orientation
+surface_type = 'fcc100'
+####################################################
+# surface atoms
+symbol = 'Cu'
+####################################################
+# lattice constant
+a = 3.6
+####################################################
+# vacuum in the z direction (Angstrem)
+vacuum = 8.0
+####################################################
+# filename of the optimized surface slab
+slabopt = 'Cu_100_slab_opt.xyz'
+####################################################
+# Quantum Espresso pseudopotantials for DFT calculations
+pseudo_dir = '/home/mgierad/espresso/pseudo'
+pseudopotentials = "dict(Cu='Cu.pbe-spn-kjpaw_psl.1.0.0.UPF', H='H.pbe-kjpaw_psl.1.0.0.UPF', O='O.pbe-n-kjpaw_psl.1.0.0.UPF', C='C.pbe-n-kjpaw_psl.1.0.0.UPF')"
+####################################################
+# Quantum chemistry package executable
+executable = '/opt/custom/espresso/6.6_nostress/bin/pw.x'
+####################################################
+# Balsam executable settings
+balsam_exe_settings = {'num_nodes': 1,
+                       'ranks_per_node': 48,
+                       'threads_per_rank': 1
+                       }
+calc_keywords = {'kpts': (3, 3, 1),
+                 'occupations': 'smearing',
+                 'smearing': 'marzari-vanderbilt',
+                 'degauss': 0.01,  # Rydberg
+                 'ecutwfc': 40,  # Rydberg
+                 'nosym': True,  # Allow symmetry breaking during optimization
+                 'conv_thr': 1e-11,
+                 'mixing_mode': 'local-TF'
+                 }
+creation_dir = Path.cwd().as_posix()
+####################################################
+# filename of the .yaml file with reactions
+yamlfile = 'reactions.yaml'
+####################################################
+# specify repeats of the surface in (x, y, z) direction
+repeats_surface = (1, 1, 4)
+####################################################
+# specify repeats of the surface in (x, y, z) direction
+repeats = (3, 4, 1)
+####################################################
+# specify the angle of TS estimate adduct rotation
+rotAngle = 60
+####################################################
+# specify the scaling factor to scale the bond distance
+# between two atoms taking part in the reaction
+scfactor = 1.4
+####################################################
+# specify the scaling factor to scale the target distance
+# i.e. the average bond distance between adsorbate and
+# the nearest surface metal atom
+scfactor_surface = 1.0
+####################################################
+# species list
+species_list = ['O', 'H']
+####################################################
+# do you want to apply the scfactor_surface to the species 1?
+scaled1 = False
+####################################################
+# do you want to apply scfactor_surface to the species 2?
+scaled2 = False
+####################################################
+'''
+####################################################
+                    Scripts
+####################################################
+'''
+slab_opt_script = '00_set_up_slab_opt.py'
+SurfaceAdsorbateScript = '01_set_up_ads.py'
+TSxtbScript = '02_set_up_TS_with_xtb.py'
+TSScript = '03_checksym_xtb_runTS.py'
+IRCScript = '04_set_up_irc.py'
+IRCoptScript = '05_set_up_opt_after_irc.py'
