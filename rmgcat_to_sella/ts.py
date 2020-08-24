2f1a776dc796b4b3050872e71fd4ba6abd774073
Maciej Gierada <maciek.gierada@gmail.com>
2020-08-23 18:41:37 -0700

    all catkit dependencies removed -- debugging

--- rmgcat_to_sella/ts.py
+++ rmgcat_to_sella/ts.py
@@ -168,7 +168,7 @@ class TS():
             images):
 == == == =
     def prepare_react_list(self):
-        '''Convert yaml file to more useful format
+    '''Convert yaml file to more useful format
 
         Returns
         _______
@@ -180,7 +180,7 @@ class TS():
             a list of CatKit's Gratom object (both reactants and products)
 
         '''
-        with open(self.yamlfile, 'r') as f:
+       with open(self.yamlfile, 'r') as f:
             yamltxt = f.read()
         reactions = yaml.safe_load(yamltxt)
         speciesInd = []
