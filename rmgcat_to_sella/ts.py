c1a64f67d7884ed53e617b3d40501ba98a384c3b
Maciej Gierada <maciek.gierada@gmail.com>
2020-08-23 18:37:21 -0700

    fixed ts.py

--- rmgcat_to_sella/ts.py
+++ rmgcat_to_sella/ts.py
@@ -1,6 +1,6 @@
-from catkit import Gratoms
-from catkit.gen.adsorption import Builder
-from catkit.build import molecule
+from rmgcat_to_sella.gratoms import Gratoms
+from rmgcat_to_sella.adsorption import Builder
+from rmgcat_to_sella.molecules import molecule, get_3D_positions
 
 from rmgcat_to_sella.adsorbates import Adsorbates
 from rmgcat_to_sella.main import WorkFlow
@@ -156,14 +156,75 @@ class TS():
         # max_rot_angle = 360/(nrot/4) # lets try 120 angle
         return max_rot_angle
 
-    def TS_placer(
-            self,
-            ts_estimate_path,
-            scfactor,
-            rxn_name,
-            r_name_list,
-            p_name_list,
-            images):
+    def prepare_react_list(self):
+        '''Convert yaml file to more useful format
+
+        Returns
+        _______
+        r_name_list : list(str)
+            a list with all reactants for the given reaction
+        p_name_list : list(str)
+            a list with all products for the given reaction
+        images : list(Gratoms)
+            a list of CatKit's Gratom object (both reactants and products)
+
+        '''
+        with open(self.yamlfile, 'r') as f:
+            yamltxt = f.read()
+        reactions = yaml.safe_load(yamltxt)
+        speciesInd = []
+        bonds = []
+        unique_species = []
+        unique_bonds = []
+        images = []
+
+        put_adsorbates = Adsorbates(
+            self.facetpath, self.slab, self.repeats, self.yamlfile,
+            self.creation_dir
+        )
+
+        for rxn in reactions:
+            # transforming reactions data to gratom objects
+            reactants, rbonds = put_adsorbates.rmgcat_to_gratoms(
+                rxn['reactant'].split('\n'))
+            products, pbonds = put_adsorbates.rmgcat_to_gratoms(
+                rxn['product'].split('\n'))
+            speciesInd += reactants + products
+            bonds += rbonds + pbonds
+
+        # check if any products are the same as any reactants
+        for species1, bond in zip(speciesInd, bonds):
+            for species2 in unique_species:
+                if nx.is_isomorphic(species1.graph, species2.graph, node_test):
+                    break
+            else:
+                images.append(get_3D_positions(species1))
+                unique_species.append(species1)
+                unique_bonds.append(bond)
+
+        r_name_list = [str(species.symbols) for species in reactants]
+        p_name_list = [str(species.symbols) for species in products]
+
+        return r_name_list, p_name_list, images
+
+    def get_rxn_name(self):
+        ''' Get the reaction name
+
+        Returns
+        _______
+        The name of the reaction in the following format:
+        OH_H+O
+        '''
+        r_name_list, p_name_list, _ = TS.prepare_react_list(self)
+
+        r_name = '+'.join([species for species in r_name_list])
+        p_name = '+'.join([species for species in p_name_list])
+
+        rxn_name = r_name + '_' + p_name
+        return rxn_name
+
+    def TS_placer(self, scfactor, rotAngle, rxn_name,
+                  r_name_list, p_name_list, images):
         ''' Place adsorbates on the surface to estimate TS
 
         Parameters:
