1957ad8e52cd45417805f006f09aa29b3522f2f6
Maciej Gierada <maciek.gierada@gmail.com>
2020-08-31 10:52:31 -0700

    running 01 step with maxstep = 0.005 Cu_111, yamlparser test many reaction

--- rmgcat_to_sella/pytemplate/pytemplate_relax_Cu_111_ads.py
+++ rmgcat_to_sella/pytemplate/pytemplate_relax_Cu_111_ads.py
@@ -52,7 +52,7 @@ atoms.calc = EspressoBalsamSocketIO(
 
 atoms.calc.set(**extra_calc_keywords)
 
-opt = QuasiNewton(atoms=atoms, maxstep=0.01, trajectory=jobdir + '.traj')
+opt = QuasiNewton(atoms=atoms, maxstep=0.005, trajectory=jobdir + '.traj')
 # opt = Sella(atoms, order=0, delta0=1e-2, trajectory=jobdir + '.traj')
 opt.run(fmax=0.01)
 atoms.calc.close()
