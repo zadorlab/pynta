3f452928aa165e5d298d0bb6d37e518398dfd75d
Maciej Gierada <maciek.gierada@gmail.com>
2020-10-22 09:31:11 -0700

    describe method print info about all jobs

--- rmgcat_to_sella/restart.py
+++ rmgcat_to_sella/restart.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from balsam.launcher.dag import BalsamJob
+from collections import Counter
 
 
 class Restart():
@@ -25,3 +26,11 @@ class Restart():
             if job.state != 'JOB_FINISHED':
                 running_jobs.append(job)
         return len(running_jobs)
+
+    def describe(self):
+        job_states = []
+        for job in self.ase.jobs:
+            job_states.append(job.state)
+        current_state = Counter(job_states)
+        for key, val in current_state.items():
+            print('{:>15} : {:>4}'.format(key, val))
