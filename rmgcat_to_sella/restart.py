4b8db7c6e974fb39d1c66a698bd6310f40a70ed4
Maciej Gierada <maciek.gierada@gmail.com>
2020-10-28 16:02:39 -0700

    get_minima_to_restart returns a list with all ase_jobs for 01 step that have to be restarted

--- rmgcat_to_sella/restart.py
+++ rmgcat_to_sella/restart.py
@@ -1,16 +1,18 @@
 import os
-from balsam.launcher.dag import BalsamJob
-from pathlib import Path
+# from balsam.launcher.dag import BalsamJob
+
+unfinished = {'05_Cu_100_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_111_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', '03_Cu_100_run_TS_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_100_run_TS_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_111_HO_03_relax.py': 'RUNNING', 'Cu_111_O_00_relax.py': 'RUNNING', '02_Cu_100_set_up_TS_with_xtb_OH_O+H.py': 'AWAITING_PARENTS', '02_Cu_111_set_up_TS_with_xtb_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_111_run_TS_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_111_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_111_run_TS_OH_O+H.py': 'AWAITING_PARENTS', '04_Cu_100_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '05_Cu_100_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_100_CH_01_relax.py': 'RUNNING',
+              'Cu_100_HO_02_relax.py': 'RUNNING', 'Cu_100_O_01_relax.py': 'RUNNING', '02_Cu_111_set_up_TS_with_xtb_OH_O+H.py': 'AWAITING_PARENTS', '05_Cu_111_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_100_HO_01_relax.py': 'RUNNING', 'Cu_100_C_01_relax.py': 'RUNNING', '02_Cu_100_set_up_TS_with_xtb_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_100_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_111_C_03_relax.py': 'RUNNING', 'Cu_111_O_01_relax.py': 'RUNNING', 'Cu_111_O_02_relax.py': 'RUNNING', 'Cu_111_O_03_relax.py': 'RUNNING', 'Cu_111_H_00_relax.py': 'RUNNING', 'Cu_111_H_01_relax.py': 'RUNNING', 'Cu_111_H_02_relax.py': 'RUNNING', 'Cu_111_H_03_relax.py': 'RUNNING', '05_Cu_111_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS'}
 
 
 class LowLevelRestart():
     def __init__(self):
         self.current_dir = os.getcwd()
         # get all python (ASE) jobs
-        self.ase_jobs = BalsamJob.objects.filter(
-            application__contains='python')
+        # self.ase_jobs = BalsamJob.objects.filter(
+        #     application__contains='python')
 
-    def jobs_to_restart(self):
+    def get_jobs_to_restart(self):
         ''' Get a dictionary with all ase_jobs that did not finish '''
         unfinished = {}
         for job in self.ase_jobs:
@@ -18,10 +20,12 @@ class LowLevelRestart():
                 unfinished[job.name] = job.state
         return unfinished
 
-    # def get_minima_to_restart(self):
-    #     minima_out_files = Path(self.current_dir).glob('**/*.out')
-    #     for mininma_out_file in minima_out_files:
-    #         print(mininma_out_file)
+    def get_minima_to_restart(self):
+        unfinished_minima = []
+        for key in unfinished.keys():
+            if 'relax' in key:
+                unfinished_minima.append(key)
+        print(unfinished_minima)
 
 
 class HighLevelRestart():
