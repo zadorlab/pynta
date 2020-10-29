9806294b3c78475647d7534443d91143dea1cc75
Maciej Gierada <maciek.gierada@gmail.com>
2020-10-29 11:34:28 -0700

    new method : prepare_ts_to_restart

--- rmgcat_to_sella/restart.py
+++ rmgcat_to_sella/restart.py
@@ -1,10 +1,12 @@
 import os
+from sys import path
 from ase.io import read, write
 from ase.io.formats import UnknownFileTypeError
+from numpy.core.fromnumeric import prod
 # from balsam.launcher.dag import BalsamJob
 
 all_unfinished = {'05_Cu_100_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_111_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', '03_Cu_100_run_TS_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_100_run_TS_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_111_HO_03_relax.py': 'RUNNING', 'Cu_111_O_00_relax.py': 'RUNNING', '02_Cu_100_set_up_TS_with_xtb_OH_O+H.py': 'AWAITING_PARENTS', '02_Cu_111_set_up_TS_with_xtb_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_111_run_TS_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_111_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '03_Cu_111_run_TS_OH_O+H.py': 'AWAITING_PARENTS', '04_Cu_100_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '05_Cu_100_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_100_CH_01_relax.py': 'RUNNING',
-                  'Cu_100_HO_02_relax.py': 'RUNNING', 'Cu_100_O_01_relax.py': 'RUNNING', '02_Cu_111_set_up_TS_with_xtb_OH_O+H.py': 'AWAITING_PARENTS', '05_Cu_111_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_100_HO_01_relax.py': 'RUNNING', 'Cu_100_C_01_relax.py': 'RUNNING', '02_Cu_100_set_up_TS_with_xtb_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_100_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_111_C_03_relax.py': 'RUNNING', 'Cu_111_O_01_relax.py': 'RUNNING', 'Cu_111_O_02_relax.py': 'RUNNING', 'Cu_111_O_03_relax.py': 'RUNNING', 'Cu_111_H_00_relax.py': 'RUNNING', 'Cu_111_H_01_relax.py': 'RUNNING', 'Cu_111_H_02_relax.py': 'RUNNING', 'Cu_111_H_03_relax.py': 'RUNNING', '05_Cu_111_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS'}
+                  'Cu_100_HO_02_relax.py': 'RUNNING', 'Cu_100_O_01_relax.py': 'RUNNING', '02_Cu_111_set_up_TS_with_xtb_OH_O+H.py': 'AWAITING_PARENTS', '05_Cu_111_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_100_HO_01_relax.py': 'RUNNING', 'Cu_100_C_01_relax.py': 'RUNNING', '02_Cu_100_set_up_TS_with_xtb_CH_C+H.py': 'AWAITING_PARENTS', '04_Cu_100_set_up_TS_vib_OH_O+H.py': 'AWAITING_PARENTS', 'Cu_111_C_03_relax.py': 'RUNNING', 'Cu_111_O_01_relax.py': 'RUNNING', 'Cu_111_O_02_relax.py': 'RUNNING', 'Cu_111_O_03_relax.py': 'RUNNING', 'Cu_111_H_00_relax.py': 'RUNNING', 'Cu_111_H_01_relax.py': 'RUNNING', 'Cu_111_H_02_relax.py': 'RUNNING', 'Cu_111_H_03_relax.py': 'RUNNING', '05_Cu_111_set_up_TS_vib_CH_C+H.py': 'AWAITING_PARENTS', '00_Cu_111_OH_O+H_ts.py': 'RUNNING'}
 
 
 class LowLevelRestart():
@@ -49,17 +51,17 @@ class LowLevelRestart():
 
         Returns
         -------
-        unfinished_TSs : list(str)
+        unfinished_tss : list(str)
             a list with all *py files for started but yet finished saddle
             point optimizations
 
         '''
         # all_unfinished = self.get_jobs_to_restart()
-        unfinished_TSs = []
+        unfinished_tss = []
         for key, value in all_unfinished.items():
             if 'ts' in key and 'AWAITING_PARENTS' not in value:
-                unfinished_TSs.append(key)
-        return unfinished_TSs
+                unfinished_tss.append(key)
+        return unfinished_tss
 
     def prepare_minima_to_restart(self):
         ''' If there is at least one optimization step in a .traj file
@@ -75,16 +77,33 @@ class LowLevelRestart():
             facetpath = metal_symbol + '_' + facet
             path_to_species = os.path.join(
                 facetpath, 'minima', species, prefix)
-            new_xyz_fname = os.path.join(
-                path_to_species + '.xyz')
 
             try:
                 # try convert last step in *traj file to a new .xyz file
-                atom_traj_file = read(path_to_species + '.traj')
-                write(new_xyz_fname, atom_traj_file)
+                write(path_to_species + '.xyz', path_to_species + '.traj')
             except UnknownFileTypeError:
                 # continue if *traj file is empty
-                # hard HighLevelRestart required
+                # hard HighLevelRestart is required
+                continue
+
+    def prepare_ts_to_restart(self):
+        unfinished_tss = self.get_tss_to_restart()
+        for ts in unfinished_tss:
+            prefix, metal_symbol, facet, reactant, product, _ = ts.split(
+                '_')
+            facetpath = metal_symbol + '_' + facet
+            rxn_name = reactant + '_' + product
+            ts_name = os.path.join(
+                prefix + '_' + facetpath + '_' + rxn_name + '_ts')
+            path_to_ts = os.path.join(
+                facetpath, rxn_name, 'TS_estimate_unique', prefix, ts_name)
+
+            try:
+                # try convert last step in *traj file to a new .xyz file
+                write(path_to_ts + '.xyz', read(path_to_ts + '.traj'))
+            except UnknownFileTypeError:
+                # continue if *traj file is empty
+                # hard HighLevelRestart is required
                 continue
 
 
