692f60df0ce1270d6dd0378161dd8de3eccd3667
Maciej Gierada <maciek.gierada@gmail.com>
2020-11-18 13:01:13 -0800

    towards new way of generating species - added new method IO.prepare_reactants_and_products(rxn)

--- pynta/io.py
+++ pynta/io.py
@@ -261,7 +261,7 @@ class IO():
             rxn['product'].split('\n'))
         species_ind += reactants + products
         bonds += rbonds + pbonds
-        # check if any products are the same as anßy reactants
+        # check if any products are the same as any reactants
         for species1, bond in zip(species_ind, bonds):
             for species2 in unique_species:
                 if nx.is_isomorphic(species1.graph, species2.graph, node_test):
@@ -274,6 +274,8 @@ class IO():
 
         r_name_list = [str(species.symbols) for species in reactants]
         p_name_list = [str(species.symbols) for species in products]
+
+        print(r_name_list)
 
         return r_name_list, p_name_list, images
 
@@ -319,147 +321,11 @@ class IO():
         return all_rxns
 
     @ staticmethod
-    def rmgcat_to_gratoms(
-            adjtxt: str) -> Tuple[List[Gratoms], List[int]]:
-        ''' Convert a slice of .yaml file to Catkit's Gratoms object
-
-        Parameters:
-        ___________
-
-        adjtxt : list
-            a list with a connectivity info for reactant or product
-            as from the .yaml file.
-            e.g. for given reaction (reactant or product)
-
-            In .yaml file we have something like that:
-
-                    multiplicity -187
-                1 *1 C u0 p0 c0 { 2,S} {4,S}
-                2    O u0 p0 c0 {1,S}
-                3 *2 H u0 p0 c0 {5,S}
-                4 *3 X u0 p0 c0 {1,S}
-                5 *4 X u0 p0 c0 {3,S}
-
-            but we need here a list like that:
-
-            ['multiplicity -187', '1 *1 C u0 p0 c0 {2,S} {4,S}',
-            '2    O u0 p0 c0 {1,S}', '3 *2 H u0 p0 c0 {5,S}',
-            '4 *3 X u0 p0 c0 {1,S}', '5 *4 X u0 p0 c0 {3,S}', '']
-
-            So it can be simply converted using the following:
-
-            yamlfile = 'reactions.yaml'
-            with open(yamlfile, 'r') as f:
-                text = f.read()
-            reactions = yaml.safe_load(text)
-            for rxn in reactions:
-                adjtxt = rxn['reactant'].split('\n')
-
-        Returns:
-        ________
-        gratoms_list : list
-            a Gratom like object
-        bonds : list
-            a list of bonds to the metal
-
-        '''
-        symbols = []
-        edges = []
-        tags = []
-        # bond_index = None
-        for i, line in enumerate(adjtxt):
-            if 'multiplicity' in line:
-                continue
-            if not line:
-                break
-
-            line = line.split()
-            inc = 0
-            if line[1][0] == '*':
-                inc = 1
-                tags.append(int(line[1][1]))
-            else:
-                tags.append(0)
-
-            symbols.append(line[1 + inc])
-            conn = line[5 + inc:]
-
-            for bond in conn:
-                j = int(bond.strip('{}').split(',')[0])
-                if j > i:
-                    edges.append((i, j - 1))
-                    # TODO bug -> change to the below for MG made reactions
-                    # edges.append((i, j - 1))
-        gratoms = Gratoms(symbols, edges=edges)
-
-        del_indices = []
-
-        for i, atom in enumerate(gratoms):
-            if atom.symbol == 'X':
-                for j in gratoms.graph.neighbors(i):
-                    tags[j] *= -1
-                del_indices.append(i)
-
-        gratoms.set_tags(tags)
-        del gratoms[del_indices]
-
-        gratoms_list = []
-        bonds = []
-        for i, subgraph in enumerate(
-            nx.connected_component_subgraphs(gratoms.graph)
-        ):
-            # TODO bug -> remove [::-1] to have a default order of atoms
-            # as they appears in reactions.yaml
-            indices = list(subgraph.nodes)[::-1]
-            # symbols = str(gratoms[indices].symbols)
-            symbols = IO.fix_symbols(str(gratoms[indices].symbols))
-            # new_gratoms = gratoms[indices].copy()
-            new_indices = {old: new for new, old in enumerate(indices)}
-            new_edges = []
-            for edge in subgraph.edges:
-                newa = new_indices[edge[0]]
-                newb = new_indices[edge[1]]
-                new_edges.append((newa, newb))
-            new_gratoms = Gratoms(symbols, edges=new_edges)
-
-            bond = None
-            tags = new_gratoms.get_tags()
-            for i, tag in enumerate(tags):
-                if tag < 0:
-                    if bond is None:
-                        bond = [i]
-                    elif len(bond) == 1:
-                        bond.append(i)
-                    else:
-                        raise RuntimeError(
-                            'At most two bonds to the metal are allowed '
-                            'per adsorbate!'
-                        )
-                    tags[i] = abs(tags[i])
-            new_gratoms.set_tags(tags)
-            bonds.append(bond)
-            gratoms_list.append(new_gratoms)
-        return gratoms_list, bonds
+    def rmgcat_to_gratoms(rxn):
+        pass
 
     @staticmethod
-    def fix_symbols(symbols):
-        # perumtations = get_permutations(list(symbols))
-        # print(perumtations)
-        # correct_symbols = {'CH3O': ['H2COH', 'HCOH2']}
-        correct_symbols = {'OH': ['HO', 'OH'],
-                           'H2O': ['H2O', 'HO2', '2HO', '2OH', 'O2H', 'OH2'],
-                           'CO2': ['CO2', 'C2O', 'OC2', 'O2C', '2OC', '2CO'],
-                           'COOH': ['CO2H', 'CHO2', 'O2CH', 'O2HC'],
-                           'CH2O': ['H2OC', 'CH2O', 'OCH2', 'H2CO'],
-                           'CH3O': ['H2COH'],
-                           'HCOO': ['CO2H', 'HCO2']}
-        for key, value in correct_symbols.items():
-            if symbols in value:
-                return key
-        return symbols
-
-    @staticmethod
-    def get_better_rxn_name(rxn):
+    def prepare_reactants_and_products(rxn):
         raw_rxn_name = rxn['reaction']
         reactants, products = raw_rxn_name.split('<=>')
 
@@ -475,8 +341,152 @@ class IO():
 
         [el.remove('X') for el in [reactants, products] if 'X' in el]
 
+        return reactants, products
+
+    @staticmethod
+    def get_better_rxn_name(rxn):
+        reactants, products = IO.prepare_reactants_and_products(rxn)
         rxn_name = '+'.join(reactants) + '_' + '+'.join(products)
         return rxn_name
+
+        #     adjtxt: str) -> Tuple[List[Gratoms], List[int]]:
+        # ''' Convert a slice of .yaml file to Catkit's Gratoms object
+
+        # Parameters:
+        # ___________
+
+        # adjtxt : list
+        #     a list with a connectivity info for reactant or product
+        #     as from the .yaml file.
+        #     e.g. for given reaction (reactant or product)
+
+        #     In .yaml file we have something like that:
+
+        #             multiplicity -187
+        #         1 *1 C u0 p0 c0 { 2,S} {4,S}
+        #         2    O u0 p0 c0 {1,S}
+        #         3 *2 H u0 p0 c0 {5,S}
+        #         4 *3 X u0 p0 c0 {1,S}
+        #         5 *4 X u0 p0 c0 {3,S}
+
+        #     but we need here a list like that:
+
+        #     ['multiplicity -187', '1 *1 C u0 p0 c0 {2,S} {4,S}',
+        #     '2    O u0 p0 c0 {1,S}', '3 *2 H u0 p0 c0 {5,S}',
+        #     '4 *3 X u0 p0 c0 {1,S}', '5 *4 X u0 p0 c0 {3,S}', '']
+
+        #     So it can be simply converted using the following:
+
+        #     yamlfile = 'reactions.yaml'
+        #     with open(yamlfile, 'r') as f:
+        #         text = f.read()
+        #     reactions = yaml.safe_load(text)
+        #     for rxn in reactions:
+        #         adjtxt = rxn['reactant'].split('\n')
+
+        # Returns:
+        # ________
+        # gratoms_list : list
+        #     a Gratom like object
+        # bonds : list
+        #     a list of bonds to the metal
+
+        # '''
+        # symbols = []
+        # edges = []
+        # tags = []
+        # # bond_index = None
+        # for i, line in enumerate(adjtxt):
+        #     if 'multiplicity' in line:
+        #         continue
+        #     if not line:
+        #         break
+
+        #     line = line.split()
+        #     inc = 0
+        #     if line[1][0] == '*':
+        #         inc = 1
+        #         tags.append(int(line[1][1]))
+        #     else:
+        #         tags.append(0)
+
+        #     symbols.append(line[1 + inc])
+        #     conn = line[5 + inc:]
+
+        #     for bond in conn:
+        #         j = int(bond.strip('{}').split(',')[0])
+        #         if j > i:
+        #             edges.append((i, j - 1))
+        #             # TODO bug -> change to the below for MG made reactions
+        #             # edges.append((i, j - 1))
+        # gratoms = Gratoms(symbols, edges=edges)
+
+        # del_indices = []
+
+        # for i, atom in enumerate(gratoms):
+        #     if atom.symbol == 'X':
+        #         for j in gratoms.graph.neighbors(i):
+        #             tags[j] *= -1
+        #         del_indices.append(i)
+
+        # gratoms.set_tags(tags)
+        # del gratoms[del_indices]
+
+        # gratoms_list = []
+        # bonds = []
+        # for i, subgraph in enumerate(
+        #     nx.connected_component_subgraphs(gratoms.graph)
+        # ):
+        #     # TODO bug -> remove [::-1] to have a default order of atoms
+        #     # as they appears in reactions.yaml
+        #     indices = list(subgraph.nodes)[::-1]
+        #     symbols = gratoms[indices].symbols
+        #     # symbols = IO.fix_symbols(str(gratoms[indices].symbols))
+        #     # new_gratoms = gratoms[indices].copy()
+        #     new_indices = {old: new for new, old in enumerate(indices)}
+        #     new_edges = []
+        #     for edge in subgraph.edges:
+        #         newa = new_indices[edge[0]]
+        #         newb = new_indices[edge[1]]
+        #         new_edges.append((newa, newb))
+        #     new_gratoms = Gratoms(symbols, edges=new_edges)
+
+        #     bond = None
+        #     tags = new_gratoms.get_tags()
+        #     for i, tag in enumerate(tags):
+        #         if tag < 0:
+        #             if bond is None:
+        #                 bond = [i]
+        #             elif len(bond) == 1:
+        #                 bond.append(i)
+        #             else:
+        #                 raise RuntimeError(
+        #                     'At most two bonds to the metal are allowed '
+        #                     'per adsorbate!'
+        #                 )
+        #             tags[i] = abs(tags[i])
+        #     new_gratoms.set_tags(tags)
+        #     bonds.append(bond)
+        #     gratoms_list.append(new_gratoms)
+        # return gratoms_list, bonds
+
+    @staticmethod
+    def fix_symbols(symbols):
+        # perumtations = get_permutations(list(symbols))
+        # print(perumtations)
+        # correct_symbols = {'CH3O': ['H2COH', 'HCOH2']}
+        correct_symbols = {'OH': ['HO', 'OH'],
+                           'H2O': ['H2O', 'HO2', '2HO', '2OH', 'O2H', 'OH2'],
+                           'CO2': ['CO2', 'C2O', 'OC2', 'O2C', '2OC', '2CO'],
+                           'COOH': ['CO2H', 'CHO2', 'O2CH', 'O2HC'],
+                           'CH2O': ['H2OC', 'CH2O', 'OCH2', 'H2CO'],
+                           'CH3O': ['H2COH'],
+                           'HCOO': ['CO2H', 'HCO2'],
+                           'CO3': ['O3C', 'CO3']}
+        for key, value in correct_symbols.items():
+            if symbols in value:
+                return key
+        return symbols
 
     @staticmethod
     def get_xyz_from_traj(
