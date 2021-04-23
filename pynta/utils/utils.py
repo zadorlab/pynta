2f94c9e87df682fb0dfd6c79d71c2fc9bc1a0437
Maciej Gierada <maciek.gierada@gmail.com>
2021-04-23 13:41:31 +0200

    feat: adding docs to utils

--- pynta/utils/utils.py
+++ pynta/utils/utils.py
@@ -12,7 +12,16 @@ edge_cases_bonded_dict = dict(CO3=1, CH3OH=1, CH3O2=2, HCO=1)
 edge_cases_topology_dict = dict(COOH=1, HCOOH=1, CH3O2=1, HCOOCH3=17)
 
 
-def convert_traj_to_xyz(path_to_traj):
+def convert_traj_to_xyz(path_to_traj: str) -> None:
+    ''' Convert all geometries in path_to_traj file to xyz format and combine
+    everything into one file
+
+    Parameters
+    ----------
+    path_to_traj : str
+        a path to *.traj file
+
+    '''
 
     traj = Trajectory(path_to_traj)
 
@@ -28,7 +37,15 @@ def convert_traj_to_xyz(path_to_traj):
             os.remove(f_name)
 
 
-def traj_to_xyz(path_to_traj):
+def traj_to_xyz(path_to_traj: str) -> None:
+    ''' Convert last geometry of path_to_traj to xyz file
+
+    Parameters
+    ----------
+    path_to_traj : str
+        a path to *.traj file
+
+    '''
     dir_name = os.path.dirname(path_to_traj)
     f_name = os.path.join(dir_name, 'last_geom.xyz')
     write(f_name, read(path_to_traj))
