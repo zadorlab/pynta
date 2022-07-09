1d6063a33c788fe30fb1b68b2324157896fbcd1c
Matt Johnson <mjohnson541@gmail.com>
2022-07-08 17:00:18 -0700

    fix

--- pynta/newmain.py
+++ pynta/newmain.py
@@ -4,7 +4,7 @@ import ase.build
 from ase.io import read, write
 import os
 import time
-import pynta.IO as IO
+from pynta.io import IO
 
 class Pynta:
     def __init__(self,path,rxns_file,surface_type,metal,a=3.6,vaccum=8.0,
