8d92b900a1178347de5a1c3f119a191d689362d8
Matt Johnson <mjohnson541@gmail.com>
2022-12-09 06:54:29 -0800

    enable specification of lattice constant to skip automatic computation

--- pynta/main.py
+++ pynta/main.py
@@ -22,7 +22,7 @@ import fireworks.fw_config
 
 class Pynta:
     def __init__(self,path,launchpad_path,fworker_path,rxns_file,surface_type,metal,label,vacuum=8.0,
-        repeats=[(1,1,1),(3,3,4)],slab_path=None,software="Espresso",socket=False,queue=False,njobs_queue=0,
+        repeats=[(1,1,1),(3,3,4)],slab_path=None,software="Espresso",socket=False,queue=False,njobs_queue=0,a=None,
         software_kwargs={'kpts': (3, 3, 1), 'tprnfor': True, 'occupations': 'smearing',
                             'smearing':  'marzari-vanderbilt',
                             'degauss': 0.01, 'ecutwfc': 40, 'nosym': True,
@@ -42,6 +42,7 @@ class Pynta:
         self.launchpad = launchpad
         self.slab_path = slab_path
         self.vacuum = vacuum
+        self.a = a
         self.software = software
         self.socket = socket
         self.repeats = repeats
@@ -101,8 +102,12 @@ class Pynta:
         """
         slab_type = getattr(ase.build,self.surface_type)
         #optimize the lattice constant
-        a = get_lattice_parameter(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
-        print("computed lattice constant of: {} Angstroms".format(a))
+        if self.a is None:
+            a = get_lattice_parameter(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
+            print("computed lattice constant of: {} Angstroms".format(a))
+            self.a = a
+        else:
+            a = self.a
         #construct slab with optimial lattice constant
         slab = slab_type(self.metal,self.repeats[1],a,self.vacuum)
         slab.pbc = (True, True, False)
