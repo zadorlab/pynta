4e54f5e0f54e6e0d2a1ade8482b6df12b0cb9525
Matt Johnson <mjohnson541@gmail.com>
2023-03-08 13:19:10 -0800

    add function for extracting the energies, thermochemistry and frequencies for a given TS directory

--- pynta/postprocessing.py
+++ pynta/postprocessing.py
@@ -71,3 +71,34 @@ def get_irc_dirs(path):
             freq_dirs.append(p)
     return freq_dirs
 
+def get_energies(path):
+    Es = dict()
+    thermos = dict()
+    fs = dict()
+    guess_dirs = os.listdir(path)
+    slab = read(os.path.join(os.path.split(path)[0],"slab.xyz"))
+    Eslab = slab.get_potential_energy()
+    for guess in guess_dirs:
+        p = os.path.join(path,guess,"irc_forward.traj")
+        if os.path.exists(p):
+            sp = read(os.path.join(path,guess,"opt.xyz"))
+            freqdir = os.path.join(path,guess,"vib.json_vib.json")
+            if not os.path.exists(freqdir):
+                Es[guess] = None
+                thermos[guess] = None
+                continue
+            vibdata = get_vibdata(os.path.join(path,guess,"opt.xyz"),freqdir,len(slab))
+            freqs = vibdata.get_frequencies()
+            fs[guess] = freqs
+            freqs = [np.complex128(x) for x in freqs]
+            ind = np.argmax(np.abs(np.imag(np.array(freqs))))
+            if len([f for f in freqs if np.imag(f) > 100]) > 1:
+                print("didn't like freqs")
+
+            ZPE = vibdata.get_zero_point_energy()
+            E = sp.get_potential_energy()
+            Es[guess] = E+ZPE-Eslab
+            thermos[guess] = (HarmonicThermo(np.array([x for x in np.real(vibdata.get_energies()) if x > 0.0]),potentialenergy=E+ZPE-Eslab))
+
+    return Es,thermos,fs
+
