51ed66c2e082dba566eea85b87414eb034e4c79e
Matt Johnson <mjohnson541@gmail.com>
2023-03-08 13:16:49 -0800

    add function for plotting the harmonic energies

--- pynta/postprocessing.py
+++ pynta/postprocessing.py
@@ -12,3 +12,23 @@ import ase.units
 from molecule.molecule import Molecule
 from acat.adsorption_sites import SlabAdsorptionSites
 
+def plot_eharm(path,Eharmtol=3.0,Eharmfiltertol=30.0):
+    eharms = []
+    guess_dirs = os.listdir(path)
+    for guess in guess_dirs:
+        try:
+            with open(os.path.join(path,guess,"harm.json")) as f:
+                out = json.load(f)
+                eharms.append(out["harmonic energy"])
+        except:
+            pass
+    eharms = sorted(eharms)
+    eharmmin = np.min(eharms)
+
+    plt.plot(eharms)
+    plt.xlabel("TS Guess in Order of Increasing Energy")
+    plt.ylabel("Harmonic Energy [eV]")
+    plt.yscale("log")
+    plt.plot(range(len(eharms)),np.ones(len(eharms))*eharmmin*Eharmtol)
+    plt.plot(range(len(eharms)),np.ones(len(eharms))*eharmmin*Eharmfiltertol)
+
