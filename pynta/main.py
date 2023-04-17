1c9a9ac61662f8865c4af6c9f683eba09956126b
sakim8048 <sakim8048@gmail.com>
2023-04-17 10:22:12 -0700

    revert main.py as master branch main.py

--- pynta/main.py
+++ pynta/main.py
@@ -24,7 +24,7 @@ import logging
 
 class Pynta:
     def __init__(self,path,rxns_file,surface_type,metal,label,launchpad_path=None,fworker_path=None,
-        vacuum=8.0,repeats=[(1,1,1),(3,3,4)],slab_path=None,software="Espresso",socket=False,queue=False,njobs_queue=0,a=None,
+        vacuum=8.0,repeats=[(1,1,1),(3,3,4)],slab_path=None,software=None,socket=False,queue=False,njobs_queue=0,a=None,
         software_kwargs={'kpts': (3, 3, 1), 'tprnfor': True, 'occupations': 'smearing',
                             'smearing':  'marzari-vanderbilt',
                             'degauss': 0.01, 'ecutwfc': 40, 'nosym': True,
@@ -49,7 +49,10 @@ class Pynta:
         self.slab_path = slab_path
         self.vacuum = vacuum
         self.a = a
-        self.software = software
+        if software:
+            self.software = software
+        else:
+            software == "Espresso"
         self.socket = socket
         self.repeats = repeats
         self.path = os.getcwd() if path is None else path
