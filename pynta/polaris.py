2f0b7f4cb9869231d7ccc3014c0d5a7039106c59
Raymundo Hernandez <rayhe88@gmail.com>
2024-03-28 15:45:15 +0000

    Change the executable and the binding cpu for pwdft

--- pynta/polaris.py
+++ pynta/polaris.py
@@ -8,12 +8,13 @@ from fireworks.core.fworker import FWorker
 def createCommand(node, software):
     binary = os.environ.get("EXE")
     if binary is None:
-        binary = '/lus/eagle/projects/catalysis_aesp/abagusetty/qe-7.1/build_cuda_nompiaware/bin/pw.x'
+        binary = '/lus/eagle/projects/catalysis_aesp/raymundo/soft/qe-7.1/build_cuda_nompiaware/bin/pw.x'
 
     if software == 'Espresso':
-        command = 'mpiexec --hosts {} -n 4 --ppn 4 --depth=8 --cpu-bind depth --env OMP_NUM_THREADS=8 --env CUDA_VISIBLE_DEVICES=0,1,2,3 {} -in PREFIX.pwi > PREFIX.pwo'.format(node, binary)
+        command = 'mpiexec --hosts {} -n 4 --ppn 4 --depth=8 --cpu-bind depth --env OMP_NUM_THREADS=8 --env CUDA_VISIBLE_DEVICES=0,1,2,3 {} -nk 4 -in PREFIX.pwi > PREFIX.pwo'.format(node, binary)
     elif software == 'PWDFT':
-        command = 'mpiexec --hosts {} -n 4 --ppn 4 --cpu-bind depth --env OMP_NUM_THREADS=1 --env CUDA_VISIBLE_DEVICES=0,1,2,3 {} PREFIX.nwxi > PREFIX.nwxo'.format(node, binary)
+        affinity = '/lus/eagle/projects/catalysis_aesp/raymundohe/bin/affinity.sh'
+        command = 'mpiexec --hosts {} -n 4 --ppn 4 --mem-bin list:0:1:2:3 --cpu-bind list:0-7:8-15:16-23:24-31 --env OMP_NUM_THREADS=1 {}  {} PREFIX.nwxi > PREFIX.nwxo'.format(node, affinity, binary)
 
     return command
 
