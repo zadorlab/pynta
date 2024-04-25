336081abc230374519642a1918305d1013f633fd
Matt Johnson <mjohnson541@gmail.com>
2024-04-24 18:23:05 -0700

    adapt get_lattice_parameter test

--- pynta/testCalculator.py
+++ pynta/testCalculator.py
@@ -4,8 +4,8 @@ from pynta.calculator import *
 
 class UtilsTest(unittest.TestCase):
 
-    def test_get_lattice_parameter(self):
-        a = get_lattice_parameter('Cu','fcc111','EMT',dict())
+    def test_get_lattice_parameters(self):
+        a = get_lattice_parameters('Cu','fcc111','EMT',dict())
         self.assertAlmostEqual(a,3.5898294220923366)
 
 if __name__ == '__main__':
