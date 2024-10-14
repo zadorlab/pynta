16cf6a0c9543cadd19da3723e7820308b2d1d7c3
Matt Johnson <mjohnson541@gmail.com>
2024-10-14 11:45:38 -0700

    avoid issues with adjlists in pairs

--- pynta/tasks.py
+++ pynta/tasks.py
@@ -1090,10 +1090,11 @@ class TrainCovdepModelTask(FiretaskBase):
         for d in calculation_directories:
             with open(os.path.join(d,"info.json"),'r') as f:
                 info = json.load(f)
-            try:
-                init_config = Molecule().from_adjacency_list(info['adjlist'],check_consistency=False)
-            except Exception as e:
-                raise ValueError((e,info['adjlist']))
+            adjlist = info['adjlist']
+            adjlist.replace('+3','0')
+            adjlist.replace('+2','0')
+            adjlist.replace('+1','0')
+            init_config = Molecule().from_adjacency_list(adjlist,check_consistency=False)
             new_computed_configs.append(init_config)
             datum_E,datums_stability = process_calculation(d,ad_energy_dict,slab,metal,facet,sites,site_adjacency,pynta_dir,coadmol_E_dict,max_dist=3.0,rxn_alignment_min=0.7,
                 coad_disruption_tol=1.1,out_file_name="out",init_file_name="init",vib_file_name="vib",is_ad=None)
