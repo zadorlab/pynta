ec3835d946a0f5254c756de8c4b87bf3ce681f67
Matt Johnson <mjohnson541@gmail.com>
2024-10-14 22:43:57 -0700

    avoid explicitly invoking sort_atoms flag

--- pynta/coveragedependence.py
+++ pynta/coveragedependence.py
@@ -2130,10 +2130,14 @@ def mol_to_atoms(admol,slab,sites,metal,partial_atoms=None,partial_admol=None):
         gpartial_admol = partial_admol.to_group()
         admol.update(sort_atoms=False,raise_atomtype_exception=False)
         admol.update_connectivity_values()
-        try:
-            subisos = admol.find_subgraph_isomorphisms(gpartial_admol,save_order=True)
-        except ValueError:
-            subisos = admol.find_subgraph_isomorphisms(gpartial_admol,save_order=True)
+        admol_atom_order = admol.atoms[:]
+        gpartial_admol_atom_order = gpartial_admol.atoms[:]
+        subisos = admol.find_subgraph_isomorphisms(gpartial_admol)
+        admol.atoms = admol_atom_order[:]
+        admol.update(sort_atoms=False,raise_atomtype_exception=False)
+        admol.update_connectivity_values()
+        gpartial_admol = gpartial_admol_atom_order[:]
+        gpartial_admol.update()
         if len(subisos) == 0:
             raise ValueError("partial_admol is not subgraph isomorphic to admol: {0}, {1}".format(gpartial_admol.to_adjacency_list(),admol.to_adjacency_list()))
         subiso = subisos[0]
