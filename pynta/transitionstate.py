f779ce0309546d296fb4c9429f03089d3c2d0469
Matt Johnson <mjohnson541@gmail.com>
2025-03-21 21:08:59 -0700

    debugging is_isomorphic

--- pynta/transitionstate.py
+++ pynta/transitionstate.py
@@ -274,7 +274,11 @@ def get_unique_TS_structs(adsorbates,species_names,slab,slab_sites,site_adjacenc
     unique_tsmols = []
     for i,m1 in enumerate(tsmols):
         for j,m2 in enumerate(unique_tsmols):
-            if m1.is_isomorphic(m2,save_order=True):
+            try:
+                iso = m1.is_isomorphic(m2,save_order=True)
+            except:
+                raise ValueError((m1.to_adjacency_list(),m2.to_adjacency_list()))
+            if iso:
                 break 
         else:
             unique_tsmols.append(m1)
