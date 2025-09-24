72a53095add68a61c1712bdf46330e8f716a812f
sakim8048 <sakim8048@gmail.com>
2025-09-24 15:42:06 -0700

    make get_lattice_parameter() function as default

--- pynta/main.py
+++ pynta/main.py
@@ -135,8 +135,8 @@ class Pynta:
         slab_type = getattr(ase.build,self.surface_type)
         #optimize the lattice constant
         if self.a is None:
-            a = optimize_lattice_parameter_with_initial(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
-            #a = get_lattice_parameter(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
+            #a = optimize_lattice_parameter_with_initial(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
+            a = get_lattice_parameter(self.metal,self.surface_type,self.software,self.lattice_opt_software_kwargs)
             print("computed lattice constant of: {} Angstroms".format(a))
             self.a = a
         else:
