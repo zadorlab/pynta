008e18190ce652db526a066a5f09cd4eded8d8ba
sakim8048 <sakim8048@gmail.com>
2026-04-23 14:48:52 -0700

    convert adjacency keys to int after loading in main.py

--- pynta/main.py
+++ pynta/main.py
@@ -200,12 +200,12 @@ class Pynta:
         """
         if sites_obj is None:
             return None
-    
+
         # Case A: already a flat list of site dicts
         if isinstance(sites_obj, list) and (len(sites_obj) == 0 or isinstance(sites_obj[0], dict)):
             if len(sites_obj) > 0 and "position" in sites_obj[0]:
                 return sites_obj
-    
+
             # Case B: list of {"geom_index":..., "sites":[...]}
             if len(sites_obj) > 0 and "sites" in sites_obj[0]:
                 flat = []
@@ -216,11 +216,18 @@ class Pynta:
                         continue
                     flat.extend(entry["sites"])
                 return flat
-    
+
         raise ValueError(
             "Unsupported sites format. Provide either a flat list of site dicts "
             "or a list of {'geom_index':..., 'sites':[...]} dicts."
         )
+
+    def _normalize_site_adjacency(self, adj):
+        if adj is None:
+            return None
+        if not isinstance(adj, dict):
+            raise ValueError(f"site_adjacency must be a dict, got {type(adj)}")
+        return {int(k): [int(x) for x in v] for k, v in adj.items()}
 
     def generate_slab(self,skip_launch=False):
         """
@@ -263,46 +270,51 @@ class Pynta:
             self.slab = slab
             write(self.slab_path,slab)
 
-    def analyze_slab(self):
-        full_slab = self.slab
+def analyze_slab(self):
+    full_slab = self.slab
 
-        # 0) If sites provided directly (e.g., dict), prefer them.
-        if self.sites is not None:
-            # If adjacency wasn't provided directly, optionally load it from file (if given)
-            if self.site_adjacency is None and self.site_adjacency_file_path is not None:
-                self.site_adjacency = self.load_json(self.site_adjacency_file_path)
-
-            if self.site_adjacency is None:
-                raise ValueError(
-                    "self.sites was provided directly, but site_adjacency is missing. "
-                    "Provide self.site_adjacency or site_adjacency_file_path."
-                )
-
-        # 1) If sites not provided directly but sites_file_path is given, load from file.
-        elif self.sites_file_path is not None:
-            self.sites = self.load_json(self.sites_file_path)
-            self.sites = self._normalize_sites(self.sites)   # added here
-
-            if self.site_adjacency_file_path is None:
-                print("Provide site_adjacency_path")
-                raise SystemExit(1)
-
+    # 0) If sites provided directly (e.g., dict), prefer them.
+    if self.sites is not None:
+        if self.site_adjacency is None and self.site_adjacency_file_path is not None:
             self.site_adjacency = self.load_json(self.site_adjacency_file_path)
-            if self.site_adjacency is None:
-                print("Provide site_adjacency_path")
-                raise SystemExit(1)
 
-        # 2) Otherwise do original ACAT autodetection.
-        else:
-            logging.info("Attempting to automatically detect sites based on metal and facet using ACAT")
-            cas = SlabAdsorptionSites(full_slab, self.surface_type, allow_6fold=False, composition_effect=False,
-                                      label_sites=True,
-                                      surrogate_metal=self.surrogate_metal)
-            self.sites = cas.get_sites()
-            self.site_adjacency = cas.get_neighbor_site_list()
+        if self.site_adjacency is None:
+            raise ValueError(
+                "self.sites was provided directly, but site_adjacency is missing. "
+                "Provide self.site_adjacency or site_adjacency_file_path."
+            )
 
-        unique_site_lists, unique_site_pairs_lists, single_site_bond_params_lists, double_site_bond_params_lists = \
-            generate_unique_placements(full_slab, self.sites)
+        self.site_adjacency = self._normalize_site_adjacency(self.site_adjacency)  # <-- add here
+        self.sites = self._normalize_sites(self.sites)  # (recommended also add here)
+
+    # 1) If sites not provided directly but sites_file_path is given, load from file.
+    elif self.sites_file_path is not None:
+        self.sites = self.load_json(self.sites_file_path)
+        self.sites = self._normalize_sites(self.sites)
+
+        if self.site_adjacency_file_path is None:
+            print("Provide site_adjacency_path")
+            raise SystemExit(1)
+
+        self.site_adjacency = self.load_json(self.site_adjacency_file_path)
+        if self.site_adjacency is None:
+            print("Provide site_adjacency_path")
+            raise SystemExit(1)
+
+        self.site_adjacency = self._normalize_site_adjacency(self.site_adjacency)  # <-- add here
+
+    # 2) Otherwise do original ACAT autodetection.
+    else:
+        logging.info("Attempting to automatically detect sites based on metal and facet using ACAT")
+        cas = SlabAdsorptionSites(full_slab, self.surface_type, allow_6fold=False, composition_effect=False,
+                                  label_sites=True, surrogate_metal=self.surrogate_metal)
+        self.sites = cas.get_sites()
+        self.site_adjacency = cas.get_neighbor_site_list()
+
+        self.site_adjacency = self._normalize_site_adjacency(self.site_adjacency)  # <-- optional (safe)
+
+    unique_site_lists, unique_site_pairs_lists, single_site_bond_params_lists, double_site_bond_params_lists = \
+        generate_unique_placements(full_slab, self.sites)
 
         self.single_site_bond_params_lists = single_site_bond_params_lists
         self.single_sites_lists = unique_site_lists
