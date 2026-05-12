99cb40902265c0ef95cac1683ef748c0e4449766
Judit Zador <jzador@sandia.gov>
2026-05-12 11:01:56 -0700

    more options for list error

--- pynta/calculator.py
+++ pynta/calculator.py
@@ -464,9 +464,10 @@ def map_harmonically_forced(input):
             d = {"harmonic energy": Eharm, "harmonic force": Fharm.tolist(),"atom_bond_potentials":atom_bond_potentials,
                  "site_bond_potentials":s_bond_potentials,"molecule_to_atom_maps":molecule_to_atom_maps,"ase_to_mol_num":ase_to_mol_num}
             json.dump(d,f)
-        for key, val in list(sp.arrays.items()):
-            if isinstance(val, list):
-                sp.arrays[key] = np.array(val)
+        for container in (sp.arrays, sp.info):
+            for key in list(container.keys()):
+                if isinstance(container[key], list):
+                    container[key] = np.array(container[key])
         write(os.path.join(path,str(j),"xtb.xyz"),sp)
         xyz = os.path.join(path,str(j),"xtb.xyz")
         return (sp.todict(),Eharm,xyz)
