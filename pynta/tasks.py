350fa8d75ad03a11fcb90380010cd85ad992e90f
Matt Johnson <mjohnson541@gmail.com>
2026-05-18 12:27:22 -0700

    supply sites, morphologies, atoms, radical electrons and lone pairs to covdep training

--- pynta/tasks.py
+++ pynta/tasks.py
@@ -1243,6 +1243,11 @@ class TrainCovdepModelTask(FiretaskBase):
         nslab = len(slab)
         allowed_structure_site_structures = generate_allowed_structure_site_structures(os.path.join(pynta_dir,"Adsorbates"),sites,site_adjacency,nslab,max_dist=np.inf)
         
+        r_site = [""] + list(set([site["site"] for site in sites]))
+        r_morph = [""] + list(set([site["morphology"] for site in sites]))
+        r_atoms = list(set([a.element.symbol for admol in admol_name_structure_dict.keys() for a in admol.atoms]))
+        r_un = list(set([a.radical_electrons for admol in admol_name_structure_dict.keys() for a in admol.atoms]))
+        r_lone_pairs = list(set([a.lone_pairs for admol in admol_name_structure_dict.keys() for a in admol.atoms]))
         
         nodes_file = os.path.join(os.path.split(pynta.models.__file__)[0],"finetuned_to_dft_delta_model.json")
         nodes_isolated = read_nodes(nodes_file)
@@ -1277,9 +1282,6 @@ class TrainCovdepModelTask(FiretaskBase):
         from pynta.coveragedependence import adsorbate_interaction_decomposition
         nodes_file = os.path.join(os.path.split(pynta.models.__file__)[0],"finetuned_to_dft_delta_model.json")
         nodes_covdep = read_nodes(nodes_file)
-        r_site = ["","ontop","bridge","hcp","fcc"]
-    
-        r_atoms = ["C","O","N","H","X"]
         sidt_finetuned_to_covdep = MultiEvalSubgraphIsomorphicDecisionTreeRegressor([adsorbate_interaction_decomposition],
                                                         nodes=nodes_covdep,
                                                         r=[ATOMTYPES[x] for x in r_atoms],
@@ -1383,8 +1385,8 @@ class TrainCovdepModelTask(FiretaskBase):
             
         Nconfigs = len(admol_name_structure_dict)
         Ncoads = 1
-        tree = train_sidt_cov_dep_regressor(pairs_datums,sampling_datums,r_site=None,
-                            r_atoms=None,node_fract_training=0.7)
+        tree = train_sidt_cov_dep_regressor(pairs_datums,sampling_datums,r_site=r_site,r_morph=r_morph,
+                            r_atoms=r_atoms,r_un=r_un,r_lone_pairs=r_lone_pairs,node_fract_training=0.7)
         
         tree_file = os.path.join(path,"Iterations",str(iter),"regressor.json")
         write_nodes(tree,tree_file)
